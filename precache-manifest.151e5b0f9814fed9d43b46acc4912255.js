1fb3f774d90cdc60b0b43d2e6ce85316a1f2b04f
Tamal Anwar <tamalanwaronline@gmail.com>
2019-09-04 20:30:09 +0600

    Updates

---
+++ precache-manifest.151e5b0f9814fed9d43b46acc4912255.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074894a1217fe36d1313a2bce4725c6a",
+    "url": "/treasure-chest/index.html"
+  },
+  {
+    "revision": "9ec7ebd9fbd555eb4b5d",
+    "url": "/treasure-chest/static/css/main.a5f40420.chunk.css"
+  },
+  {
+    "revision": "f914a4030706f5a90a97",
+    "url": "/treasure-chest/static/js/2.1bc89d3e.chunk.js"
+  },
+  {
+    "revision": "9ec7ebd9fbd555eb4b5d",
+    "url": "/treasure-chest/static/js/main.13e7e20b.chunk.js"
+  },
+  {
+    "revision": "57c657946c39a41c95ed",
+    "url": "/treasure-chest/static/js/runtime~main.3a850dba.js"
+  },
+  {
+    "revision": "e46715545db7784e868038152ef17d1f",
+    "url": "/treasure-chest/static/media/chest-close.e4671554.png"
+  },
+  {
+    "revision": "ab9dc4d9f4870f560d4c203a7b3347de",
+    "url": "/treasure-chest/static/media/gold-chest.ab9dc4d9.png"
+  },
+  {
+    "revision": "65af1706d8c3c92ab27062c5f3743d88",
+    "url": "/treasure-chest/static/media/half-chest.65af1706.png"
+  },
+  {
+    "revision": "e023663f788a7b576e663cf32c091b15",
+    "url": "/treasure-chest/static/media/silver-chest.e023663f.png"
+  }
+]);
